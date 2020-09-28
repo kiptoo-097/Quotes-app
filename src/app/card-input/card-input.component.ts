f6a76aaab488fd0c9906aff09cdf6a8e1bed1448
kiptoo-097 <kiptoo097@gmail.com>
2020-09-28 21:01:58 +0300

    added new quote parameters

--- src/app/card-input/card-input.component.ts
+++ src/app/card-input/card-input.component.ts
@@ -7,25 +7,26 @@ import { Quote } from '../quote';
   styleUrls: ['./card-input.component.css']
 })
 export class CardInputComponent implements OnInit {
-  quotes = [
-    new Quote('Peter Theil', 'Genius', 'We live in a world in which courage is in less supply than genius', 'Kennedy', new Date(2020, 7, 7)),
-    new Quote('Ludwing van beetoken', 'Music', 'Music is a higher revelation than all wisdom and philosophy', 'Peter Theil', new Date(2020, 8, 21)),
+  quotes:Quote[] = [
+    // new Quote('Peter Theil', 'Education', 'The more that you read, the more things you will know, the more that you learn, the more places you’ll go.', ' Dr. Seuss', new Date(2020, 9, 7)),
+     new Quote('Kiptoo Kennedy','Education', ' Education is not preparation for life; education is life itself.', 'John Dewey', new Date(2020, 9, 21)),
+    new Quote('Hakim','Education', 'Music expresses that which cannot be said and on which it is impossible to be silent.', 'Victor Hugo', new Date(2020, 9, 21)),
   ]
-  i: number;
-  constructor() { }
+  // i: number;
+  // constructor() { }
 
-  ngOnInit(): void {
-  }
-  thumbsup(i) {
-    this.quotes[i].thumbsup++
-  }
-  thumbsdown(i) {
-    this.quotes[i].thumbsdown++
-  }
-  saveQuote(newQuote) {
-    this.quotes.push(newQuote);
-  }
-  delete(i){
-    this.quotes.splice(i, 1);
-  }
+  // ngOnInit(): void {
+  // }
+  // thumbsup(i) {
+  //   this.quotes[i].thumbsup++
+  // }
+  // thumbsdown(i) {
+  //   this.quotes[i].thumbsdown++
+  // }
+  // saveQuote(newQuote) {
+  //   this.quotes.unshift(newQuote);
+  // }
+  // delete(i){
+  //   this.quotes.splice(i, 1);
+  // }
 }
